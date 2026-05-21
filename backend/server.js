bf3fb861c176c8b35555a751b294b44629217ad8
menukahansda <menukahansda199@gmail.com>
2026-05-21 22:13:32 +0530

    Add try and catch to the function

--- backend/server.js
+++ backend/server.js
@@ -5,13 +5,18 @@ import app from './app.js';
 import connectDB from './config/database.js';
 
 const startServer = async() =>{
-    await connectDB();
+    try{
+        await connectDB();
+        console.log("MongoDB connected successfully");
+        const PORT = process.env.API_PORT || 4000;
 
-    const PORT = process.env.API_PORT || 4000;
-
-    app.listen(PORT, ()=>{
-        console.log(`Server is running on port ${PORT}`);
-    })
+        app.listen(PORT, ()=>{
+            console.log(`Server running on port ${PORT}`);
+        })
+    }catch(err){
+        console.log("Failed to start server", err);
+        process.exit(1);
+    }
 }
 
 startServer();
