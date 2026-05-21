8e8d245f6fd82302654d1cd54ad444a6405ded3d
menukahansda <menukahansda199@gmail.com>
2026-05-21 22:11:16 +0530

    Update user model

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,12 +1,9 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-    firstName: {type : String, required: true},
-    lastName: {type : String, required: true},
     username: {type : String, required: true, unique: true},
-    email: {type : String, required: true, unique: true},
-    password: {type : String, required: true},
-    token: {type : String},
+    email: {type : String, required: true, unique: true, lowercase: true, trim: true},
+    password: {type : String, required: true, select: false},
     role: {type : String, enum: ["user", "admin"], default: "user"},
 }, {timestamps: true});
 
