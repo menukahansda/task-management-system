6934f1e3ccaba696e444c852cdc2fbd300240498
menukahansda <menukahansda199@gmail.com>
2026-05-23 01:13:22 +0530

    Update the server api url with env variable instead of localhost

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import { getToken } from "../utils/auth";
 
-const BASE_URL = "http://localhost:4000/api/v1";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:4000/api/v1";
 
 const request = async (endpoint, options = {}) => {
   const token = getToken();
