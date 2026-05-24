9be791455bec1053c7014de20443a8998ecd212a
menukahansda <menukahansda199@gmail.com>
2026-05-24 16:35:20 +0530

    Add root GET route

--- backend/app.js
+++ backend/app.js
@@ -10,8 +10,13 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/tasks', taskRoutes);
+
+app.get("/", (req, res)=>{
+    res.status(200).send(`Go to ${process.env.FRONTEND_URL} to view the frontend`);
+});
 
 app.get("/welcome", auth, (req, res)=>{
     res.status(200).send("Welcome 🙌 ");
