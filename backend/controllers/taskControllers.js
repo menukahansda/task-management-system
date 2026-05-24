cfbd187eb4d3a0fcd1be57ce4b69429fdf267447
menukahansda <menukahansda199@gmail.com>
2026-05-24 16:49:35 +0530

    fix: resolve Mongoose deprecation warnings in task update API

--- backend/controllers/taskControllers.js
+++ backend/controllers/taskControllers.js
@@ -65,7 +65,9 @@ export const updateTask = async (req, res) => {
         const task = await Task.findOneAndUpdate({
             _id: req.params.id,
             user: req.user.id
-        }, req.body, {new: true});
+            }, req.body, {
+            returnDocument: "after"
+        }); 
         if(!task)
             return res.status(404).json({
                 success: false,
